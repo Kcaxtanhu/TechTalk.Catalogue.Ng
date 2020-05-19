bce3840ae9dee5b40ec475cde5c554a485fe6758
SABINO Damásio <damasio.sabino@amaris.com>
2020-05-19 11:40:33 +0100

    Updating folders

--- ttt-presentation/src/app/app.module.ts
+++ ttt-presentation/src/app/app.module.ts
@@ -7,12 +7,19 @@ import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SessoesComponent } from './sessoes/sessoes.component';
-import { SessaoDetalheComponent } from './sessao-detalhe/sessao-detalhe.component';
+import { SessaoDetalheComponent } from './sessoes/sessao-detalhe/sessao-detalhe.component';
 import { ApresentadoresComponent } from './apresentadores/apresentadores.component';
-import { ApresentadorDetalheComponent } from './apresentador-detalhe/apresentador-detalhe.component';
+import { ApresentadorDetalheComponent } from './apresentadores/apresentador-detalhe/apresentador-detalhe.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, SessoesComponent, SessaoDetalheComponent, ApresentadoresComponent, ApresentadorDetalheComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    SessoesComponent,
+    SessaoDetalheComponent,
+    ApresentadoresComponent,
+    ApresentadorDetalheComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
